e8c1af14b3deee9f4a0ef9045d535cf6dd01fdad
codesith <codesith@gmail.com>
2016-10-08 20:16:37 -0700

    Shuffle more

--- main.go
+++ main.go
@@ -7,27 +7,38 @@ import (
 
 func main() {
 	d := new(card.Deck)
-	trials := 10
+	trials := 999123
 	c := 0
 	for i := 0; i < trials; i++ {
 		d.Init()
-		d.Shuffle()
-		c = c + countKings(d)
+		for j := 0; j < 10; j++ {
+			d.Shuffle()
+		}
+		//d.PrintDeck()
+		c = c + countKings(d, i)
+		printProgress(trials, i)
 	}
-	fmt.Printf("Average times is %.2f\n", float64(c)/float64(trials))
+	fmt.Printf("\r%.5f\n", float64(c)/float64(trials))
 }
 
-func countKings(d *card.Deck) (count int) {
+func countKings(d *card.Deck, round int) (count int) {
 	count = 0
 	for i := 0; i < 52; i++ {
 		card := d.GetCard(i)
 		if i < 51 {
 			nextCard := d.GetCard(i + 1)
 			if card.V == 13 && nextCard.V == 13 {
-				fmt.Printf("%d%s and %d%s are next to each other\n", card.V, card.S, nextCard.V, nextCard.S)
+				//fmt.Printf("Round%d: %d/%s,%d/%s\n", round, card.V, card.S, nextCard.V, nextCard.S)
 				count++
 			}
 		}
 	}
 	return
+}
+
+func printProgress(trials int, i int) {
+	bucketSize := trials / 100
+	if i%bucketSize == 0 {
+		fmt.Printf("\r%d%%", (i/bucketSize + 1))
+	}
 }
